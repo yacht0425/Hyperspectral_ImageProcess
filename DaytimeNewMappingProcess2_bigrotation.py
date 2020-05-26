3ce92b179b49fc7756d9448c970afc9217849f7d
yacht0425 <59048306+yacht0425@users.noreply.github.com>
2020-05-26 12:22:58 +0900

    端の処理や次のrowへの切り替え，最後の処理など改良
    けれど，最後のCoodinateがうまくいかないから今のところボツ．．．

--- DaytimeNewMappingProcess2_bigrotation.py
+++ DaytimeNewMappingProcess2_bigrotation.py
@@ -39,51 +39,123 @@ os.chdir(directory_all_HS)
 path = os.getcwd()
 HS_files = os.listdir(directory_all_HS)
 flen = len(HS_files)
-print(flen)
+print('file number is %d' %flen)
 initial_degree = ip.FirstDirection(HS_files[0],gps) #initial degree means initial yaw using rotation
 initial_yaw = ip.firstYaw(HS_files[0],imu) #This is for yaw angle from second image on
-print(initial_degree)
+print('Initial degree is %f' %initial_degree)
 
 
-#Read first png image
-os.chdir(directory_image)
-initial_image = cv2.imread('0.png')
-initial_image = cv2.rotate(initial_image,cv2.ROTATE_180) #I don't know why but completed images are opposite so modify here
 #Concatenate images first row 
 j = 0
+j_list = []
 for k in range(flen):
+    #Read first image
+    os.chdir(directory_image)
+    initial_image = cv2.imread('%d.png' %j)
+    print('Next initial image is %d' %j)
+    
+    #concatenate images as the number of images 
     for i in range(number - 1):
         os.chdir(directory_image)
-        image = cv2.imread('%s.png' %i)
-        image = cv2.rotate(image,cv2.ROTATE_180) #I don't know why but completed images are opposite so modify here
-        if i == 0:
-            all_image = np.append(initial_image,image, axis=0)
-            print(all_image.shape)
-        else:
-            all_image = np.append(all_image,image, axis=0)
-            print(all_image.shape)
-            
         j = j + 1
-        print('j = %d' %j)  
+        print('j = %d' %j)
         
-        #Confirm line working or turning. If turning then break
-        os.chdir(directory_all_HS) #Move to directory of HS files
+        image = cv2.imread('%s.png' %j)
+        if i == 0: #The first append is initial and next image, the second one is all_images and next image
+            all_images = np.append(initial_image,image, axis=0)
+            print(all_images.shape)
+        else:
+            all_images = np.append(all_images,image, axis=0)
+            print(all_images.shape)
+        
+        #Confirm line working or turning. If turning, then check the number by the end and concatenate the number of images
+        os.chdir(directory_all_HS)
         degree = ip.Yaw(HS_files[j],imu,initial_degree,initial_yaw)
-        past_degree = ip.Yaw(HS_files[j-1],imu,initial_degree,initial_yaw)
-        diference = abs(degree - past_degree)
-        print('Degree deviation = %f' %diference)
-        if diference > 10:
+        next_degree = ip.Yaw(HS_files[j+number-1],imu,initial_degree,initial_yaw) #next_degree means the last image in concatenated images 
+        difference = abs(degree - next_degree)
+        print('Degree deviation = %f' %difference)
+        #The process of the end of the row 
+        if difference > 10:
+            del all_images
+            last_number = j
+            os.chdir(directory_image)
+            initial_image = cv2.imread('%d.png' %j)
+            print('Last initial image is %d' %j)
+            #Detect the number of remain images 
+            while True:
+                os.chdir(directory_all_HS) #Move to directory of HS files
+                degree = ip.Yaw(HS_files[last_number],imu,initial_degree,initial_yaw)
+                next_degree = ip.Yaw(HS_files[last_number+1],imu,initial_degree,initial_yaw)
+                difference = abs(degree - next_degree)
+                if difference < 5:
+                    print('It is fine.')
+                else:
+                    print('Change point is detected')
+                    break
+                last_number = last_number + 1
+            a = last_number - j
+            for i in range(a):
+                os.chdir(directory_image)
+                j = j + 1
+                print('j = %d' %j) 
+                image = cv2.imread('%s.png' %j)
+                if i == 0:
+                    all_images = np.append(initial_image,image, axis=0)
+                    print(all_images.shape)
+                else:
+                    all_images = np.append(all_images,image, axis=0)
+                    print(all_images.shape)
+                
+                
+            '''
+            all_images = cv2.rotate(all_images,cv2.ROTATE_180)
+            y_pixel = len(all_images)
+            print(y_pixel)
+            os.chdir(directory_all_HS)
+            degree = ip.BigDirection(HS_files[j-a],HS_files[j],gps)
+            print(degree)
+            print(j-a)
+            print(j)
+            '''
+            j_list.append(j)
+            '''
+            rotated_all_images = ip.big_rotation(all_images,degree,x_pixel,y_pixel_for_last)   
+                
+            os.chdir(directory_image_rotated)
+            cv2.imwrite('big%d.png' %k, rotated_all_images)
+            '''
+            
+            del all_images    
+            
             print('Finish the first row')
             break
     else:
-        y_pixel = len(all_image)
+        '''
+        all_images = cv2.rotate(all_images,cv2.ROTATE_180) 
+        y_pixel = len(all_images)
+        '''
+        y_pixel_for_last = len(all_images)
+        '''
         print(y_pixel)
-        degree = ip.BigDirection(HS_files[k],HS_files[k+5],gps)
+        
+        degree = ip.BigDirection(HS_files[j-4],HS_files[j],gps)
         print(degree)
-        rotated_all_image = ip.big_rotation(all_image,degree,x_pixel,y_pixel)   
+        print(j-4)
+        print(j)
+        '''
+        j_list.append(j)
+        '''
+        rotated_all_images = ip.big_rotation(all_images,degree,x_pixel,y_pixel)   
             
         os.chdir(directory_image_rotated)
-        cv2.imwrite('big%d.png' %k, rotated_all_image)
+        cv2.imwrite('big%d.png' %k, rotated_all_images)
+        print('big%d.png done' %k)
+        '''
+        del all_images
+        
+        j = j + 1
+        
+        
         continue
     break
 
@@ -91,126 +163,189 @@ for k in range(flen):
 for l in range(row-1):
     
     #Eliminate turning image
+    n = j
+    t = 0
     for m in range(10):
-        j = j + 1
+        n = n + 1
+        t = t + 1
         os.chdir(directory_all_HS) #Move to directory of HS files
-        initial_degree = ip.FirstDirection(HS_files[j],gps) #initial degree means initial yaw using rotation
-        initial_yaw = ip.firstYaw(HS_files[j],imu) #This is for yaw angle from second image on
-        degree = ip.Yaw(HS_files[j],imu,initial_degree,initial_yaw)
-        past_degree = ip.Yaw(HS_files[j-1],imu,initial_degree,initial_yaw)
-        past_past_degree = ip.Yaw(HS_files[j-2],imu,initial_degree,initial_yaw)
+        initial_degree = ip.FirstDirection(HS_files[n],gps) #initial degree means initial yaw using rotation
+        initial_yaw = ip.firstYaw(HS_files[n],imu) #This is for yaw angle from second image on
+        degree = ip.Yaw(HS_files[n],imu,initial_degree,initial_yaw)
+        next_degree = ip.Yaw(HS_files[n+1],imu,initial_degree,initial_yaw)
+        next_next_degree = ip.Yaw(HS_files[n+2],imu,initial_degree,initial_yaw)
         print('j = %d' %j)
-        print(abs(degree - past_degree))
-        print(abs(past_degree - past_past_degree))
-        if abs(degree - past_degree) < 5 and abs(past_degree - past_past_degree) < 5: #If angle change suttles down, then break.
+        print(abs(degree - next_degree))
+        print(abs(next_degree - next_next_degree))
+        if abs(degree - next_degree) < 5 and abs(next_degree - next_next_degree) < 5: #If angle change suttles down, then break.
             print('Go to next row!')
             break
         
-    os.chdir(directory_image)
-    initial_image = cv2.imread('%d.png' %j)
-    initial_image = cv2.rotate(initial_image,cv2.ROTATE_180) #I don't know why but completed images are opposite so modify here
-
-    n = j + 1       
-    for i in range(n,flen):
+        
+    #Concatenate next row images
+    j = j + t
+    for u in range(flen):
+        k = k + 1 #This is the number of image: k.png
+        print('%d th png is making' %k)
+        os.chdir(directory_image)
+        print('Next initial image is %d' %j)
+        initial_image = cv2.imread('%d.png' %j)
         for i in range(number - 1):
-            os.chdir(directory_image)
-            image = cv2.imread('%s.png' %i+n)
-            print(i+n)
-            image = cv2.rotate(image,cv2.ROTATE_180) #I don't know why but completed images are opposite so modify here
-            if i == 0:
-                all_image = np.append(initial_image,image, axis=0)
-                print(all_image.shape)
-            else:
-                all_image = np.append(all_image,image, axis=0)
-                print(all_image.shape)
-                
             j = j + 1
-            print('j = %d' %j)  
+            print('j = %d' %j)
+            os.chdir(directory_image)
+            image = cv2.imread('%d.png' %j)
+            if i == 0:
+                all_images = np.append(initial_image,image, axis=0)
+                print(all_images.shape)
+            else:
+                all_images = np.append(all_images,image, axis=0)
+                print(all_images.shape)
             
-            #Confirm line working or turning. If turning then break
+            #Confirm line working or turning. If turning then break after concatenating remain images
             os.chdir(directory_all_HS) #Move to directory of HS files
             degree = ip.Yaw(HS_files[j],imu,initial_degree,initial_yaw)
-            past_degree = ip.Yaw(HS_files[j-1],imu,initial_degree,initial_yaw)
+            past_degree = ip.Yaw(HS_files[j-1],imu,initial_degree,initial_yaw) #next_degree means the last image in concatenated images 
             difference = abs(degree - past_degree)
             print('Degree deviation = %f' %difference)
             if difference > 10:
-                print('Finish the first row')
-                break
-        else:
-            y_pixel = len(all_image)
-            print(y_pixel)
-            degree = ip.BigDirection(HS_files[k],HS_files[k+5],gps)
-            print(degree)
-            rotated_all_image = ip.big_rotation(all_image,degree,x_pixel,y_pixel)   
-                
-            os.chdir(directory_image_rotated)
-            cv2.imwrite('big%d.png' %k, rotated_all_image)
-            
-            remain = flen - j
-            
-            if remain < number:
-                n = j
-                for i in range(remain):
-                    os.chdir(directory_image)
-                    image = cv2.imread('%s.png' %i+n)
-                    print(i+n)
-                    image = cv2.rotate(image,cv2.ROTATE_180) #I don't know why but completed images are opposite so modify here
-                    if i == 0:
-                        all_image = np.append(initial_image,image, axis=0)
-                        print(all_image.shape)
+                del all_images
+                last_number = j
+                os.chdir(directory_image)
+                initial_image = cv2.imread('%d.png' %j)
+                print('Last initial image in this row is %d' %j)
+                #Detect the number of remain images 
+                while True:
+                    os.chdir(directory_all_HS) #Move to directory of HS files
+                    degree = ip.Yaw(HS_files[last_number],imu,initial_degree,initial_yaw)
+                    next_degree = ip.Yaw(HS_files[last_number+1],imu,initial_degree,initial_yaw)
+                    difference = abs(degree - next_degree)
+                    if difference < 5:
+                        print('It is fine.')
                     else:
-                        all_image = np.append(all_image,image, axis=0)
-                        print(all_image.shape)
-                        
-                    j = j + 1
-                    print('j = %d' %j)  
+                        print('Change point is detected')
+                        break
+                    last_number = last_number + 1
+                a = last_number - j   
+                for i in range(a):
+                    os.chdir(directory_image)
+                    print('Next initial image is %d' %j)
+                    image = cv2.imread('%s.png' %i)
+                    if i == 0:
+                        all_images = np.append(initial_image,image, axis=0)
+                        print(all_images.shape)
+                    else:
+                        all_images = np.append(all_images,image, axis=0)
+                        print(all_images.shape)
                     
-                  
-                y_pixel = len(all_image)
+                    j = j + 1
+                    print('j = %d' %j) 
+                '''
+                all_images = cv2.rotate(all_images,cv2.ROTATE_180)
+                y_pixel = len(all_images)
                 print(y_pixel)
-                degree = ip.BigDirection(HS_files[k],HS_files[k+remain],gps)
-                rotated_all_image = ip.big_rotation(all_image,degree,x_pixel,y_pixel)   
+                os.chdir(directory_all_HS)
+                degree = ip.BigDirection(HS_files[j-a],HS_files[j],gps)
+                print(degree)
+                print(j-a)
+                print(j)
+                '''
+                j_list.append(j)
+                '''
+                rotated_all_images = ip.big_rotation(all_images,degree,x_pixel,y_pixel_for_last)   
                     
                 os.chdir(directory_image_rotated)
-                cv2.imwrite('big%d.png' %k, rotated_all_image)
+                cv2.imwrite('big%d.png' %k, rotated_all_images)
+                '''
+                del all_images    
+                    
+                print('Finish current row')
+                break
+        else:
+            '''
+            all_images = cv2.rotate(all_images,cv2.ROTATE_180) 
+            y_pixel = len(all_images)
+            print(y_pixel)
+            degree = ip.BigDirection(HS_files[j-4],HS_files[j],gps)
+            print(degree)
+            print(j-4)
+            print(j)
+            '''
+            j_list.append(j)
+            '''
+            rotated_all_images = ip.big_rotation(all_images,degree,x_pixel,y_pixel)   
                 
+            os.chdir(directory_image_rotated)
+            s = j // number
+            cv2.imwrite('big%d.png' %k, rotated_all_images)
+            print('big%d.png done' %k)
+            '''
+            remain = flen - 1 - j
+            print('remain is %d' %remain)
+            j = j + 1
+                  
+            if remain < number:
+                os.chdir(directory_image)
+                initial_image = cv2.imread('%d.png' %j)
+                print('Next initial image is %d' %j)
+                for i in range(remain-1):
+                    j = j + 1
+                    print('j = %d' %j)  
+                    os.chdir(directory_image)
+                    image = cv2.imread('%d.png' %j)
+                    if i == 0:
+                        all_images = np.append(initial_image,image, axis=0)
+                        print(all_images.shape)
+                    else:
+                        all_images = np.append(all_images,image, axis=0)
+                        print(all_images.shape)
+                        
+                '''    
+                initial_image = cv2.rotate(initial_image,cv2.ROTATE_180)   
+                y_pixel = len(all_images)
+                print(y_pixel)
+                os.chdir(directory_all_HS)
+                degree = ip.BigDirection(HS_files[flen-1-remain],HS_files[flen-1],gps)
+                print(flen-1-remain)
+                print(flen-1)
+                '''
+                j_list.append(j)
+                '''
+                rotated_all_images = ip.big_rotation(all_images,degree,x_pixel,y_pixel_for_last)   
+                    
+                os.chdir(directory_image_rotated)
+                s = k + 1
+                cv2.imwrite('big%d.png' %s, rotated_all_images)
+                '''
                 print('Finish all process')
                 break
             
         continue
     break
 
-'''
+j_list = np.array(j_list)
+print(j_list)
+
+image_len = len(j_list)
+w = 1280
+original_y_pixel = 1024*number
+rotated_y_pixel = int(round(np.sqrt(np.power(w/2,2) + np.power(original_y_pixel/2,2)) * 2))
+print(rotated_y_pixel)
 with open(kml_file_name,mode='w') as f:
     f.write('<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">\n')
     f.write('<Folder>\n')
     f.write('    <name>%s</name>\n' %kml_name) 
     f.write('    <open>1</open>\n')
-    coordinate = ip.imageCoordinate_for_modify(HS_files[0],gps,offset_distance,initial_degree,scanrate,velocity) 
-    imagename = '0.png'
-    f.write('    <GroundOverlay>\n')
-    f.write('        <name>0</name>\n')
-    f.write('        <Icon>\n')
-    f.write('            <href>%s</href>\n' % imagename)
-    f.write('            <viewBoundScale>0.75</viewBoundScale>\n')
-    f.write('        </Icon>\n')
-    f.write('        <LatLonBox>\n')
-    f.write('            <north>%f</north>\n' %coordinate[0])
-    f.write('            <south>%f</south>\n' %coordinate[2])
-    f.write('            <east>%f</east>\n' %coordinate[3])
-    f.write('            <west>%f</west>\n' %coordinate[1])
-    f.write('        </LatLonBox>\n')
-    f.write('    </GroundOverlay>\n')        
-    for i in range(flen - 1):
-        t = i + 1
-        yaw = ip.Yaw(HS_files[t],imu,initial_degree,initial_yaw)
-        coordinate = ip.imageCoordinate_for_modify(HS_files[t],gps,offset_distance,yaw,scanrate,velocity) ###CHANGE POINT
-        imagename = '%d.png' %t
-        f.write('    <GroundOverlay>\n')    
-        f.write('        <name>%d</name>\n' %t)
+    for i in range(3):
+        os.chdir(directory_all_HS)
+        degree = ip.BigDirection(HS_files[j_list[i]-4],HS_files[j_list[i]],gps)
+        coordinate = ip.imageCoordinate_for_big(HS_files[j_list[i]],gps,offset_distance,degree,scanrate,velocity,original_y_pixel,rotated_y_pixel) ###CHANGE POINT
+        imagename = 'big%d.png' %i
+        f.write('    <GroundOverlay>\n')
+        f.write('        <name>%d</name>\n' %i)
         f.write('        <Icon>\n')
         f.write('            <href>%s</href>\n' % imagename)
-        f.write('            <viewBoundScale>0.75</viewBoundScale>\n')
+        f.write('            <viewBoundScale>1.00</viewBoundScale>\n')
         f.write('        </Icon>\n')
         f.write('        <LatLonBox>\n')
         f.write('            <north>%f</north>\n' %coordinate[0])
@@ -220,6 +355,3 @@ with open(kml_file_name,mode='w') as f:
         f.write('        </LatLonBox>\n')
         f.write('    </GroundOverlay>\n')        
     f.write('</Folder>\n</kml>')
-'''
-
-
